33d849ce5865cd645507c6b671038fe6a5cc1b09
이인수 <in63119@gmail.com>
2023-10-22 12:22:40 +0900

    Fix: 순서 변경

--- practice2/index.js
+++ practice2/index.js
@@ -7,9 +7,9 @@ const web3 = new Web3(rpcURL);
 const abi = require("./abi.json");
 const ca = "컨트랙트 주소"; // Contract Address
 const account = "내 주소"; // 내 Address
-const contract = new web3.eth.Contract(abi, ca);
-
 const privateKey = process.env.PRIVATEKEY; // .env.example에 자신의 비밀키를 넣으세요. 파일명에서 '.example'을 꼭 지우고 사용하세요! (.env 만 사용)
+
+const contract = new web3.eth.Contract(abi, ca);
 const message = "세상아 안녕"; // HelloWorld 대신 변경하고픈 메시지를 넣으세요.
 
 const getHelloWorld = async () => {
